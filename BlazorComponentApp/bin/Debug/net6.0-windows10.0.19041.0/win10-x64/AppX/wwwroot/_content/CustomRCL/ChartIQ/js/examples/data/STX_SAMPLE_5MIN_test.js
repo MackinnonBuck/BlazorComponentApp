be8a6a02fe24f40a281dac1a4ff8939c3720c2cc
linkaiyu <lyu11111@hotmail.com>
2022-11-04 19:40:44 -0400

    update ChartIQ2 location

--- BlazorComponentApp/bin/Debug/net6.0-windows10.0.19041.0/win10-x64/AppX/wwwroot/_content/CustomRCL/ChartIQ/js/examples/data/STX_SAMPLE_5MIN_test.js
+++
@@ -1,5 +0,0 @@
-const sample5min=[
-    {"Date":"2015-04-16 16:00","Open":152.13,"High":152.19,"Low":151.47,"Close":155.11,"Volume":4505569},
-    {"Date":"2015-04-16 16:10","Open":153.5,"High":151.35,"Low":151.08,"Close":155.11,"Volume":4505569}
-];
-export default sample5min;
